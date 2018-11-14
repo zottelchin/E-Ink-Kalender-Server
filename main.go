618108643a3072c31faf74b6d189c914e8894b88
Unknown <zottelchin@gmail.com>
2018-11-14 10:32:27 +0100

    outsource into function, use gin to serve string

--- main.go
+++ main.go
@@ -3,17 +3,33 @@ package main
 import (
 	"fmt"
 	"sort"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/gin-gonic/gin"
 
 	"github.com/PuloV/ics-golang"
 )
 
 func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, stamp()+next10Events(""))
+	})
+	r.Run(":6342")
+}
+
+func stamp() string {
+	return strconv.Itoa(time.Now().Day()) + ". " + time.Now().Month().String() + " " + strconv.Itoa(time.Now().Year()) + ";"
+}
+
+func next10Events(url string) string {
 	parser := ics.New()
 	ics.RepeatRuleApply = true
 	ics.MaxRepeats = 200
 	inputChan := parser.GetInputChan()
-	inputChan <- ""
+	inputChan <- url
 	parser.Wait()
 	cal, err := parser.GetCalendars()
 	if err == nil {
@@ -35,14 +51,17 @@ func main() {
 			return allEvents[i].GetStart().Before(allEvents[j].GetStart())
 		})
 
+		returnString := ""
 		for i, event := range allEvents {
-			if i >= 10 {
+			if i >= 5 {
 				break
 			}
-			fmt.Printf("%s am %d.%s.%d\n", event.GetSummary(), event.GetStart().Day(), event.GetStart().Month().String(), event.GetStart().Year())
+			if strings.HasPrefix(event.GetSummary(), "Ü") {
+				event.SetSummary("U" + event.GetSummary()[2:])
+			}
+			returnString += fmt.Sprintf(" %s; am %d.%d von %d:%02d bis %d:%02d Uhr; Ort: %s;", event.GetSummary(), event.GetStart().Day(), event.GetStart().Month(), event.GetStart().Hour(), event.GetStart().Minute(), event.GetEnd().Hour(), event.GetEnd().Minute(), event.GetLocation())
 		}
-
-		fmt.Println("Das sind die nächsten 10 Termine")
-
+		return returnString
 	}
+	return ""
 }
