18d934549e203870d51f45b1091a9df5f1297215
Unknown <zottelchin@gmail.com>
2018-11-27 20:29:13 +0100

    Add Config and Cache to File

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"sort"
 	"strconv"
 	"time"
+
+	"teahub.io/momar/config"
 
 	"github.com/gin-gonic/gin"
 
@@ -12,18 +15,67 @@ import (
 )
 
 func main() {
+	go async()
 	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.String(200, stamp()+next10Events(""))
+	r.GET("/EA", func(c *gin.Context) {
+		content, err := ioutil.ReadFile("/var/E-Ink/cache.txt")
+		if err != nil {
+			c.String(500, stamp()+"Error; ............; ............; Datei konnte; .......; ........; nicht gelesen werden; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ; ;")
+		} else {
+			c.String(200, string(content))
+		}
 	})
 	r.Run(":6342")
+}
+
+func async() {
+	for {
+		fmt.Println("Update Data....")
+		everything := []ics.Event{}
+		c := config.Open("/var/E-Ink/config.yaml")
+		for _, d := range c.Get("Server").AnyList() {
+			for k, e := range d.AnyMap() {
+				if k == "ics" {
+					everything = append(everything, next5Events(e.String())...)
+				}
+			}
+		}
+
+		//Sortiere die Termine
+		sort.Slice(everything, func(i, j int) bool {
+			return everything[i].GetStart().Before(everything[j].GetStart())
+		})
+
+		returnString := ""
+		for i, event := range everything {
+			if i >= 5 {
+				break
+			}
+
+			if len(event.GetDTZID()) == 0 {
+				returnString += fmt.Sprintf(" %s; am %d.%d von %d:%02d bis %d:%02d Uhr; Ort: %s;", event.GetSummary(), event.GetStart().Local().Day(), event.GetStart().Local().Month(), event.GetStart().Local().Hour(), event.GetStart().Local().Minute(), event.GetEnd().Local().Hour(), event.GetEnd().Local().Minute(), event.GetLocation())
+			} else {
+				returnString += fmt.Sprintf(" %s; am %d.%d von %d:%02d bis %d:%02d Uhr; Ort: %s;", event.GetSummary(), event.GetStart().Day(), event.GetStart().Month(), event.GetStart().Hour(), event.GetStart().Minute(), event.GetEnd().Hour(), event.GetEnd().Minute(), event.GetLocation())
+			}
+
+		}
+
+		err := ioutil.WriteFile("/var/E-Ink/cache.txt", []byte(stamp()+returnString), 0644)
+		if err != nil {
+			fmt.Println("Error writing File")
+			time.Sleep(time.Minute)
+			continue
+		}
+		fmt.Println("Next Data Collection in 7 Min: " + time.Now().Add(7*time.Minute).Format("Jan 2 15:04:05 2006"))
+		time.Sleep(7 * time.Minute)
+	}
 }
 
 func stamp() string {
 	return strconv.Itoa(time.Now().Day()) + ". " + time.Now().Month().String() + " " + strconv.Itoa(time.Now().Year()) + ";"
 }
 
-func next10Events(url string) string {
+func next5Events(url string) []ics.Event {
 	parser := ics.New()
 	ics.RepeatRuleApply = true
 	ics.MaxRepeats = 200
@@ -38,32 +90,13 @@ func next10Events(url string) string {
 		//Füge alle zukünftigen und aktuellen Termine in ein Array zusammen
 		for _, calendar := range cal {
 			//  Alle Events, die noch nicht zuende sind werden gesammelt
-			for _, event := range calendar.GetUpcomingEvents(5) {
+			for _, event := range calendar.GetUpcomingEvents(7) {
 				if now.Before(event.GetEnd()) {
 					allEvents = append(allEvents, event)
 				}
 			}
 		}
-
-		//Sortiere die Termine
-		sort.Slice(allEvents, func(i, j int) bool {
-			return allEvents[i].GetStart().Before(allEvents[j].GetStart())
-		})
-
-		returnString := ""
-		for i, event := range allEvents {
-			if i >= 5 {
-				break
-			}
-
-			if len(event.GetDTZID()) == 0 {
-				returnString += fmt.Sprintf(" %s; am %d.%d von %d:%02d bis %d:%02d Uhr; Ort: %s;", event.GetSummary(), event.GetStart().Local().Day(), event.GetStart().Local().Month(), event.GetStart().Local().Hour(), event.GetStart().Local().Minute(), event.GetEnd().Local().Hour(), event.GetEnd().Local().Minute(), event.GetLocation())
-			}else {
-				returnString += fmt.Sprintf(" %s; am %d.%d von %d:%02d bis %d:%02d Uhr; Ort: %s;", event.GetSummary(), event.GetStart().Day(), event.GetStart().Month(), event.GetStart().Hour(), event.GetStart().Minute(), event.GetEnd().Hour(), event.GetEnd().Minute(), event.GetLocation())
-			}
-
-		}
-		return returnString
+		return allEvents
 	}
-	return ""
+	return nil
 }
